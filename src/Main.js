35ce5e72b99445f010cde203ee3d97c6be56eb29
Shosuke Doi <sho@Shosukes-MacBook-Pro.local>
2022-03-15 00:42:36 -0700

    first commit

---
+++ src/Main.js
@@ -0,0 +1,26 @@
+//ApiQuery.jsx
+
+import { useEffect, useState } from 'react';
+
+export const ApiQuery = (async) => {
+    const API_KEY = process.env.REACT_APP_NEWS;
+    const [newsList, setNewsList] = useState([]);
+
+    useEffect(() => {
+        getNews();
+    }, []);
+
+    const getNews = () => {
+        fetch(API_KEY)
+            .then((response) => response.json())
+            .then(({articles}) => setNewsList(articles));
+    };
+
+    return (
+        <div>
+            {newsList.map((item) => (
+                <p key={item.id}>{item.title}</p>
+            ))}
+        </div>
+    );
+};
